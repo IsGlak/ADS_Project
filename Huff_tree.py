433208071e73f7d7094eb0e7dac78caabe7b671a
IsGlak <ismael.romero@alum.esci.upf.edu>
2023-03-14 15:40:43 +0100

    def node to info_node

--- Huff_tree.py
+++ Huff_tree.py
@@ -2,5 +2,5 @@ class Huff_tree(object):
     def __init__(self,left=None,right=None):
         self.left=left
         self.right=right
-    def node(self):
+    def info_node(self):
         return(self.left,self.right)
