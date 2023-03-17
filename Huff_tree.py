0ef6cddf019c6d788d8d7939400c9e5d25edaa9f
Mikix088 <miquel.gallardo@alum.esci.upf.edu>
2023-03-17 23:13:50 +0100

    tree function

--- Huff_tree.py
+++ Huff_tree.py
@@ -14,4 +14,13 @@ def item_prob(itemList):
             probDic[i]+=1
     return probDic
 
+def tree(nod):
+    while len(nod)> 1:
+        (a,a1) = nod[-1]
+        (b,b1) = nod[-2]
+        nod = nod[:-2]
+        nod = Huff_tree(a,b)
+        nod.append(X, a1+b1) ##### missing variable 
+        nod = sorted(nod, key=lambda x: x[1], reverse = True)
+    return nod[0][0]
 
